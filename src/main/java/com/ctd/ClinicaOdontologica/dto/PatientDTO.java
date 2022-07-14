ecce21485325106672966992094d1f5f2b8096b4
ffraga98 <ferfraga2@gmail.com>
2022-07-13 21:16:14 -0300

    "ADD: Model Patient."

---
+++ src/main/java/com/ctd/ClinicaOdontologica/dto/PatientDTO.java
@@ -0,0 +1,43 @@
+package com.ctd.ClinicaOdontologica.dto;
+
+import com.ctd.ClinicaOdontologica.model.Appointment;
+import com.ctd.ClinicaOdontologica.model.Residence;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class PatientDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String DNI;
+    private Residence home;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate registrationDate;
+
+
+    public PatientDTO(Long id, String firstName, String lastName, String DNI, Residence home, LocalDate date) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DNI = DNI;
+        this.home = home;
+        this.registrationDate = date;
+    }
+
+    @JsonIgnore
+    public boolean isInvalid() {
+        return (this == null || firstName.isEmpty() || lastName.isEmpty() || DNI.isEmpty());
+    }
+
+}
---
+++ src/main/java/com/ctd/ClinicaOdontologica/model/Patient.java
@@ -0,0 +1,69 @@
+package com.ctd.ClinicaOdontologica.model;
+
+import com.ctd.ClinicaOdontologica.dto.PatientDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String DNI;
+    @Column
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate registrationDate;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "home_id")
+    private Residence home;
+
+    @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<Appointment> appointments = new HashSet<>();
+
+    @JsonIgnore
+    public boolean isInvalid() {
+        boolean result = false;
+
+        if (this == null) {
+            result = true;
+        } else if (firstName == null || lastName == null || DNI == null) {
+            result = true;
+        } else if (DNI.isEmpty() || firstName.isEmpty() || lastName.isEmpty()) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public Patient(Long id, String firstName, String lastName, Residence home, String DNI, LocalDate date) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.home = home;
+        this.DNI = DNI;
+        this.registrationDate = date;
+    }
+
+    @JsonIgnore
+    public Long getHomeId() {
+        return home.getId();
+    }
+}
