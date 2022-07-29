1a911d7b6b7d2b37a6a70194c3fb2434f46c18dc
ffraga98 <ferfraga2@gmail.com>
2022-07-29 19:06:19 -0300

    patient.html completed. postPatient.js completed

---
+++ src/main/resources/static/scripts/postPatient.js
@@ -0,0 +1,52 @@
+window.onload = () => {
+    const formulario = document.getElementById("form_patient");
+
+    formulario.addEventListener("submit", event => {
+        event.preventDefault();
+        addPatient();
+    });
+}
+
+function addPatient() {
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const dni = document.getElementById("dni");
+    const street = document.getElementById("street");
+    const number = document.getElementById("number");
+    const location = document.getElementById("location");
+    const province = document.getElementById("province");
+
+    const formData = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      dni: dni.value,
+      residence: {
+          street: street.value,
+          number: number.value,
+          location: location.value,
+          province: province.value,
+      }
+    };
+
+    const settings = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    fetch("http://localhost:8080/patients/", settings)
+      .then((response) => response.json())
+      .then(() => alert("Patient added"))
+      .catch(() => alert("Oops! Failed to load patient"))
+    //   .finally(() => {
+    //       firstName.value = "";
+    //       lastName.value = "";
+    //       dni.value = "";
+    //       street.value = "";
+    //       number.value = "";
+    //       location.value = "";
+    //       province.value = "";
+    //   })
+}
