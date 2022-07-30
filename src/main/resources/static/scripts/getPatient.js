55aad582efcd6d1d83f7b9d3b012b6a393f3ec1b
ffraga98 <ferfraga2@gmail.com>
2022-07-29 23:39:57 -0300

    Fix: getPatients.js

--- src/main/resources/static/scripts/getPatient.js
+++ src/main/resources/static/scripts/getPatient.js
@@ -10,17 +10,17 @@ window.onload = () => {
       .getElementsByTagName("tbody")[0];
   
       patients.forEach((patient) => {
-        const { domicilio } = patient;
+        const { residence } = patient;
         patientTable.innerHTML += `
           <tr>
               <td scope="row">${patient.id}</td>
               <td>${patient.firstName}</td>
               <td>${patient.lastName}</td>
               <td>${patient.dni}</td>
-              <td class="d-none d-xl-table-cell">${domicilio.street}</td>
-              <td class="d-none d-xl-table-cell">${domicilio.number}</td>
-              <td class="d-none d-lg-table-cell">${domicilio.location}</td>
-              <td class="d-none d-md-table-cell">${domicilio.province}</td>
+              <td class="d-none d-xl-table-cell">${residence.street}</td>
+              <td class="d-none d-xl-table-cell">${residence.number}</td>
+              <td class="d-none d-lg-table-cell">${residence.location}</td>
+              <td class="d-none d-md-table-cell">${residence.province}</td>
               <td>${patient.registrationDate}</td>
               <td><button class="btn btn-outline-danger" onClick="updatePatient(${patient.id})"><i class="fa-solid fa-trash-can"></i></button>
               <button class="btn btn-outline-primary" onClick="deletePatient(${patient.id})"><i class="fa-solid fa-pen-to-square"></i></button></td>
